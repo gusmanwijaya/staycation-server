13568a3a5093960bdee4c30b99e5313548886b58
Gusman Wijaya <gusman.w.jaya@gmail.com>
2021-09-11 23:04:39 +0700

    Dashboard management

--- controller/admin/DashboardController.js
+++ controller/admin/DashboardController.js
@@ -1,3 +1,7 @@
+const Member = require("../../model/Member");
+const Booking = require("../../model/Booking");
+const Item = require("../../model/Item");
+
 module.exports = {
   index: async (req, res) => {
     try {
@@ -11,11 +15,18 @@ module.exports = {
         message: alertMessage,
       };
 
+      const member = await Member.find();
+      const booking = await Booking.find();
+      const item = await Item.find();
+
       res.render("admin/dashboard/view_dashboard", {
         username: req.session.user.username,
         title: "Dashboard",
         url: originalUrl[1],
         alert,
+        member,
+        booking,
+        item,
       });
     } catch (error) {
       req.flash("alertStatus", "danger");
